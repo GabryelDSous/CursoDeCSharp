6fa946506bea185934ae03dd6ded92417e71863b
unknown <gabryelpro260406@gmail.com>
2024-12-24 16:20:43 -0300

    aula 06

---
+++ CusoDeC#/AmbienteM01/M01A06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string escola = "Estudonauta";
+            Console.WriteLine("Eu estudo no escola "+escola);
+            Console.ReadKey();  
+        }
+    }
+}
